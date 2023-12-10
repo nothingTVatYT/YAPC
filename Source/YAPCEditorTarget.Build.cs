7b65662c8d3c6967a942165a186ee1e1b3afbfe8
nothingTVatYT <rjakob13@googlemail.com>
2023-12-10 13:45:50 +0100

    fix structure

---
+++ Source/YAPCEditorTarget.Build.cs
@@ -0,0 +1,14 @@
+﻿using Flax.Build;
+
+public class YAPCEditorTarget : GameProjectEditorTarget
+{
+    /// <inheritdoc />
+    public override void Init()
+    {
+        base.Init();
+
+        // Reference the modules for editor
+        Modules.Add("YAPC");
+        Modules.Add("YAPCEditor");
+    }
+}
